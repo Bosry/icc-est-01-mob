2f7ec0d7c5cf20b16027349bef634f85d9aeae77
Bosry <b0sry2020xd@gmai.com>
2024-09-25 12:55:47 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -13,5 +13,14 @@ burbuja.printArreglo(arregloOrdenado);
 int[] arregloOrdDes = burbuja.sortByBubbleDes(arreglo);
 
 burbuja.printArregloDes(arregloOrdDes);
+
+//Ordenar las palabras alfabeticamente de la cadena de texto
+String cadena = "esto es un ejemplo de texto para Ordenar";
+String [] palabras = cadena.split(" ");
+String [] palabrasOrdenadas = burbuja.sortByBubblePalabras(palabras);
+burbuja.printArregloPalabras(palabrasOrdenadas);
+
+
+
     }
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -44,4 +44,26 @@ public Metodos(){
         }
     }
 
+    public String[] sortByBubblePalabras(String[] palabras) {
+        int tamanio = palabras.length;
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = i + 1; j < tamanio; j++) {
+                if (palabras[i].compareToIgnoreCase(palabras[j])>0) {
+                    String aux = palabras[i];
+                    palabras[i] = palabras[j];
+                    palabras[j] = aux;
+                }
+            }
+        }
+        return palabras;
+    }
+    public void printArregloPalabras(String[] palabras) {
+        System.out.println("");
+        for (String elemento : palabras) {
+            System.out.print(elemento + " ");
+        }
+    }
+
+
+
 }
