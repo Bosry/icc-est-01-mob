45ac6d34f50448c176c1ec51471240394d2943ae
Bosry <b0sry2020xd@gmai.com>
2024-09-25 12:34:36 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+Metodos burbuja = new Metodos();
+
+
+int[] arreglo = {5, 7, 30, 12, 9};
+
+int[] arregloOrdenado = burbuja.sortByBubble(arreglo);
+
+burbuja.printArreglo(arregloOrdenado);
+
+int[] arregloOrdDes = burbuja.sortByBubbleDes(arreglo);
+
+burbuja.printArregloDes(arregloOrdDes);
     }
 }
